08dfc26f5b866bb4da3e652ae7a9a6a8aab3e123
Mausami Dangi <mausamidangi@gmail.com>
2019-11-29 14:35:06 +0530

    Home page changes

--- src/Components/TabScreens/InstaHomeScreen.js
+++ src/Components/TabScreens/InstaHomeScreen.js
@@ -4,14 +4,44 @@ import {
     Text,
     FlatList,
     StyleSheet,
-    Image,
+    Image, TouchableOpacity,
 } from 'react-native';
 import COLORS from '../../Constant/Colours';
 import TopNavigationBar from '../../Common Components/TopNavigationBar'
 import DotIcon from 'react-native-vector-icons/Entypo'
+import HeartIcon from 'react-native-vector-icons/AntDesign'
+import CommentIcon from 'react-native-vector-icons/Fontisto'
+import SendIcon from 'react-native-vector-icons/Feather';
+
+
 import {heightPercentageToDP as hp, widthPercentageToDP as wp} from "react-native-responsive-screen";
 
 const statusDataArray = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        name: 'Mausami',
+        image: 'https://thumbs.dreamstime.com/z/lady-isolated-bright-background-happy-student-white-blouse-professional-lady-excited-girl-success-concept-smiling-96471759.jpg'
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        name: 'Kalgi',
+        image: 'https://lh6.googleusercontent.com/-Tg5Xvb5MZws/AAAAAAAAAAI/AAAAAAAAE00/JA2nKK6zIwc/photo.jpg'
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        name: 'Priya',
+        image: 'https://mcmscache.epapr.in/post_images/website_197/post_12824851/thumb.jpg'
+    },
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28b5',
+        name: 'Janvi',
+        image: 'https://st1.bollywoodlife.com/wp-content/uploads/2018/07/Jhanvi-kapoor-bio-wiki.jpg'
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f61',
+        name: 'Suhagi',
+        image: 'https://in.bmscdn.com/iedb/artist/images/website/poster/large/kareena-kapoor-khan-1151-26-07-2018-11-14-31.jpg'
+    },
     {
         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
         name: 'Mausami',
@@ -68,13 +98,25 @@ export default class InstaHomeScreen extends Component {
         return (
 
                 <View style={{height: hp('50%'), backgroundColor: 'red'}}>
-                    <View style={{flex: 0.13, backgroundColor: COLORS.BLACK_COLOR, alignItems: 'center', flexDirection: 'row'}}>
+                    <View style={{flex: 0.12, backgroundColor: COLORS.BLACK_COLOR, alignItems: 'center', flexDirection: 'row'}}>
                         <Image style={styles.profileIcon}
                                 source={{uri: item.image}}/>
                         <Text style={{color: COLORS.WHITE_COLOR, marginHorizontal: hp('1.5%')}}>{item.name}</Text>
-                        <DotIcon name="dots-three-horizontal" color={COLORS.WHITE_COLOR} size={hp('2%')} style={{alignSelf: 'flex-end'}}/>
+                        <DotIcon name="dots-three-horizontal" color={COLORS.WHITE_COLOR} size={hp('2%')} style={{position: 'absolute', right: hp('1%')}} />
+                    </View>
+                    <View style={{flex: 0.65, backgroundColor: 'pink'}}>
+                        <Image style={{flex: 1}}
+                               source={{uri: item.image}}/>
+                    </View>
+                    <View style={{flex: 0.09, backgroundColor: 'blue', alignItems: 'center', flexDirection: 'row'}}>
+                        <HeartIcon name="hearto" color={COLORS.WHITE_COLOR} size={hp('2.5%')} style={{marginLeft: hp('1.5%')}} />
+                        <CommentIcon name="comment" color={COLORS.WHITE_COLOR} size={hp('2.5%')} style={{marginLeft: hp('2%')}}/>
+                        <SendIcon name="send" color={COLORS.WHITE_COLOR} size={hp('2.5%')} style={{marginLeft: hp('2%')}}/>
+
+                        <DotIcon name="dots-three-horizontal" color={COLORS.WHITE_COLOR} size={hp('2%')} style={{position: 'absolute', right: hp('1%')}} />
                     </View>
 
+                    <View style={{flex: 0.14, backgroundColor: 'green'}}/>
                 </View>
 
         );
