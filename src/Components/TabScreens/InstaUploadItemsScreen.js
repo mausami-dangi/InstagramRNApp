1803fb2a7ac0dbe4089c3906102bb0d9c15cc83c
Mausami Dangi <mausamidangi@gmail.com>
2019-12-10 10:46:01 +0530

    Camera Integration

--- src/Components/TabScreens/InstaUploadItemsScreen.js
+++ src/Components/TabScreens/InstaUploadItemsScreen.js
@@ -2,21 +2,80 @@ import React, {Component} from 'react';
 import {
     View,
     StyleSheet,
-    SafeAreaView
+    Text,
+    SafeAreaView, TouchableOpacity
 } from 'react-native';
 import COLORS from '../../Constant/Colours';
+import {RNCamera} from 'react-native-camera';
+import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
 export default class InstaUploadItemsScreen extends Component {
+
+    constructor (){
+        super()
+        this.state = {selectedTab: 0}
+    }
+
     render() {
+        const {segmentContainerView, segmentView, segmentText} = styles;
         return (
-            <View style={{flex: 1, backgroundColor: COLORS.BACKGROUND_COLOR}}>
-            </View>
+            <SafeAreaView style={{flex: 1}}>
+                <SafeAreaView style={{flex: 1}}>
+                    <View style={{flex: 1}}>
+                        {
+                            (this.state.selectedTab === 0) &&
+                            <View style={{height: '95%', width: '100%', backgroundColor: 'blue'}}>
+
+                            </View> ||
+                            <View style={{flex: 1, backgroundColor: 'green'}}>
+
+                            </View>
+                        }
+
+                        <View style={segmentContainerView}>
+                            <TouchableOpacity style={[
+                                segmentView,
+                                {borderBottomWidth: (this.state.selectedTab === 0) && hp(0.2) || hp(0)}]}
+                                              onPress={() => this.setState({selectedTab: 0})}>
+                                <Text style={[
+                                    segmentText,
+                                    {fontWeight: (this.state.selectedTab === 0) && 'bold' || ''},
+                                ]}>{'GALLERY'}</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={[
+                                segmentView,
+                                {borderBottomWidth: (this.state.selectedTab === 1) && hp(0.2) || hp(0)}]}
+                                              onPress={() => this.setState({selectedTab: 1})}>
+                                <Text style={[
+                                    segmentText,
+                                    {fontWeight: (this.state.selectedTab === 1) && 'bold' || ''},
+                                ]}>{'PHOTO'}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    segmentContainerView:{
+        position: 'absolute',
+        bottom: 0,
+        height: '5%',
+        width: '100%',
+        flexDirection: 'row'
+    },
+    segmentView: {
+        backgroundColor: COLORS.HEADER_COLOR,
+        height: '100%',
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    segmentText: {
+        color: COLORS.BLACK_COLOR,
+        fontSize: hp(1.5)
     }
 });
